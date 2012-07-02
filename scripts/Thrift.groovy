0288472ae2474b359395f2247348fafbee6afc9f
aalmiray <aalmiray@gmail.com>
2012-07-02 15:24:02 +0200

    Fix src code compilation

--- scripts/Thrift.groovy
+++ scripts/Thrift.groovy
@@ -72,7 +72,7 @@ Make sure you have a similar setting on your griffon-app/conf/BuildConfig.groovy
     ant.mkdir(dir: projectMainClassesDir)
     ant.echo(message: "[thrift] Compiling generated sources to $projectMainClassesDir")
     String classpathId = "griffon.compile.classpath"
-    compileProjectSources(griffonSettings.baseDir, projectMainClassesDir, classpathId) {
+    compileProjectSources(projectMainClassesDir, classpathId) {
         src(path: gensrcDirPath)
         javac(classpathref: classpathId)
     }
